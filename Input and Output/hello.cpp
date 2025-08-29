06af1cfc25de489fa9270e1a32d0073f0e98a1ac
toryo <toryokodogbe270@gmail.com>
2025-08-29 11:21:19 +0100

    Initial commit: Set up repository structure and Visual Studio 2022 solution

---
+++ Input and Output/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // This is the C++ standard library containing important functions for I/O
+
+//This is the main() function where all of the program's code is executed from
+int main()
+{
+	std::cout << "Hello, World!";
+	return 0; // this is a check, that the main function does to show that the program executed correctly
+	// If the main function doesn't 'return' 0 there is an error in the code
+}
